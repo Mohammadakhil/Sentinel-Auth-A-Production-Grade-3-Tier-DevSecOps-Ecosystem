82dd2f50c5cdb4dfef29d314ab4303db445b1f59
Mahammad Akhil <87204024+Mohammadakhil@users.noreply.github.com>
2026-03-12 02:30:21 +0530

    Update inbound SSH rule to allow all IPv4 addresses
    Changed the CIDR for inbound SSH access to allow all IPv4 addresses.

--- terraform/security.tf
+++ terraform/security.tf
@@ -41,7 +41,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   from_port   = 22
   ip_protocol = "tcp"
   to_port     = 22
-  cidr_ipv4   = "${chomp(data.http.my_ip.response_body)}/32"
+  cidr_ipv4   = "0.0.0.0/0"
 }
 
 #resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
