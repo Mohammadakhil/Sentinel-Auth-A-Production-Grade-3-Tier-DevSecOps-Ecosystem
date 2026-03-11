1258499b51d908514b65c65ff10a996868f88f23
Mahammad Akhil <87204024+Mohammadakhil@users.noreply.github.com>
2026-03-12 02:56:36 +0530

    Fix cidr_blocks format in security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "web_server_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = "0.0.0.0/0"
   }
 }
 
@@ -69,7 +69,7 @@ resource "aws_security_group" "database_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = "0.0.0.0/0"
   }
 
 }
